8aaa2426026ee58ee6342f945d47ec4371bd4f88
Jeffrey Hirai <jhirai20@gmail.com>
2020-08-28 14:38:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SinglyLinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Single Linked List are Ideal for creating a que for say a playlist.
+            SinglyLinkedList list=new SinglyLinkedList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list.Find(2).Value) ;
+            list.PrintValues();
+            list.Remove();
+            list.PrintValues();
+        }
+    }
+}
---
+++ SinglyLinkedList.cs
@@ -0,0 +1,94 @@
+using System;
+
+namespace SinglyLinkedLists
+{
+    public class SinglyLinkedList
+    {
+        public SllNode Head;
+        public SinglyLinkedList() 
+        {
+            // your code here
+            Head=null;
+        }
+        // SLL methods go here. As a starter, we will show you how to add a node to the list.
+        public void Add(int value) 
+        {
+            SllNode newNode = new SllNode(value);
+            if(Head == null) 
+            {
+                Head = newNode;
+            } 
+            else
+            {
+                SllNode runner = Head;
+                while(runner.Next != null) {
+                    runner = runner.Next;
+                }
+                runner.Next = newNode;
+            }
+        }    
+        public void Remove()
+        {
+            if(Head != null)
+            {
+                SllNode runner=Head;
+                //if list only has one node
+                if (Head.Next == null)
+                {
+                    Head=null;
+                }
+                //Multi node list
+                else
+                {
+                    while (runner.Next.Next!=null)
+                    {
+                        runner=runner.Next;
+                    }
+                    runner.Next=null;
+                }
+            }
+        }
+        public void PrintValues()
+        {
+            SllNode runner = Head;
+            if(runner!=null)
+            {
+            while(runner.Next!=null)
+            {
+                Console.WriteLine(runner.Value);
+                runner=runner.Next;
+            }
+            Console.WriteLine(runner.Value);
+            }
+            else
+            {
+                Console.WriteLine("No Nodes in SLL!");
+            }
+        }
+        public SllNode Find(int i)
+        {
+            SllNode runner =Head;
+            while(runner.Next!=null)
+            {
+                if (runner.Value==i)
+                {
+                    return runner;
+                }
+                runner=runner.Next;
+            }
+            Console.WriteLine("Value not found!");
+            return null;
+        }
+        public void RemoveAt(int n)
+        {
+            int count=0;
+            SllNode runner=Head;
+            while(runner.Next!=null)
+            {
+                
+            }
+
+
+        }
+    }
+}
